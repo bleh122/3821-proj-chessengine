cfc42e9583de9eee1b79a243c4069450277dc779
Terence Zhao <z5479939@ad.unsw.edu.au>
2025-11-15 22:45:18 +1100

    renaming some variables and adding some comments

--- src/run_engine.cpp
+++ src/run_engine.cpp
@@ -7,21 +7,12 @@
 #include <string>
 #include <unordered_set>
 
-namespace std {
-    template<>
-    struct hash<chess::Board> {
-        auto operator()(const chess::Board& board) const noexcept -> std::size_t {
-            return std::hash<std::string>{}(board.getFen());
-        }
-    };
-}
-
 int main(int argc, char** argv) {
     int num_ending_pieces;
     int max_pieces_present;
     int depth_to_mate_checked;
 
-    if (argc < 3) {
+    if (argc < 4) {
         std::cout << "Enter the number of pieces that will be present at checkmate: ";
         if (not (std::cin >> num_ending_pieces)) {
             std::cout << "Error: incorrect input type for integer\n";
@@ -53,16 +44,16 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-
-    // challenge is to enumerate a list of numbers (range 1-64) that is of length max_pieces_present
-    // that covers all possible permutations. idk more elegant solution
-
     // for now we use an unordered set of strings, because afaik gcc has some error when trying to
     // use the overriden equality operator for chess::Board for comparisons in the unordered set
     // can maybe use tries in the future since they are strings?
     auto piece_combinations = helper::generate_piece_combinations(num_ending_pieces);
+    // this is according to n + k - 1 choose k, where n = 10, k = num_ending_pieces
+    std::cout << piece_combinations.size() << "\n";
+
     auto checkmate_states = std::unordered_set<std::string>();
     for (auto i : piece_combinations) {
+        std::cout << "now generating checkmates for new piece combination\n";
         auto some_checkmates = helper::generate_checkmates_for_piece_set_for_player(i);
         for (auto j : some_checkmates) {
             checkmate_states.insert(j);
@@ -83,35 +74,45 @@ int main(int argc, char** argv) {
     // if n is odd, then the element represents it being the white players turn and there being n
     //      moves left before forced checkmate (i.e. the white player has some move to take that
     //      will allow them to force a win from that point onwards)
-    auto states_with_forceable_wins_for_white = std::vector<std::unordered_set<std::string>>{};
-    states_with_forceable_wins_for_white.emplace_back(checkmate_states);
+    auto depth_to_mate_forced_wins_for_white = std::vector<std::unordered_set<std::string>>{};
+    depth_to_mate_forced_wins_for_white.emplace_back(checkmate_states);
 
 
-    while (states_with_forceable_wins_for_white.size() < depth_to_mate_checked) {
+    while (depth_to_mate_forced_wins_for_white.size() <= depth_to_mate_checked) {
+        std::cout << "Checking for new move depth, last iteration had this many boards: " << depth_to_mate_forced_wins_for_white.back().size() << "\n";
         auto temp = std::unordered_set<std::string>();
-        for (auto i : states_with_forceable_wins_for_white.back()) {
-            if (states_with_forceable_wins_for_white.size() % 2 == 1) {
+        for (auto i : depth_to_mate_forced_wins_for_white.back()) {
+            if (depth_to_mate_forced_wins_for_white.size() % 2 == 1) {
                 // it's white's move this turn
                 // these are states where white can select a move that will result in them winning
                 auto possible_predecessor_boards = helper::generate_predecessor_board_states(i, true);
-                temp.insert(possible_predecessor_boards.begin(), possible_predecessor_boards.end());
+
+                for (auto j : possible_predecessor_boards) {
+                    // avoid recalculation for states we already know to be winning
+                    if (not all_white_player_states_with_forceable_wins.contains(j)) {
+                        temp.emplace(j);
+                    }
+                }
             } else {
                 // it's black's move this turn
+                // these are states where whatever move black takes, they will lose in the end
                 auto possible_predecessor_boards = helper::generate_predecessor_board_states(i, false);
                 for (auto j: possible_predecessor_boards) {
                     if (helper::is_forced_win(j, all_white_player_states_with_forceable_wins)) {
-                        temp.emplace(j);
+                        if (not all_white_player_states_with_forceable_wins.contains(j)) {
+                            temp.emplace(j);
+                        }
                     }
                 }
                 // break;
             }
         }
-        states_with_forceable_wins_for_white.emplace_back(temp);
+        depth_to_mate_forced_wins_for_white.emplace_back(temp);
         all_white_player_states_with_forceable_wins.insert(temp.begin(), temp.end());
     }
 
     auto counter = 0;
-    for (auto i : states_with_forceable_wins_for_white) {
+    for (auto i : depth_to_mate_forced_wins_for_white) {
         std::cout << "\n\n\nDEPTH TO MATE: " << counter << "\n";
         for (auto j : i) {
             std::cout << "FEN: " << j << "\n";
