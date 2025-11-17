edfda58add04fe91f3bc83cfaf806e8d345aa962
Terence Zhao <z5479939@ad.unsw.edu.au>
2025-11-17 20:42:41 +1100

    added another program that reads the output of our tablebase generator, and then asks the user for their current board state, and repeatedly provides optimal moves

--- src/run_engine.cpp
+++ src/run_engine.cpp
@@ -6,54 +6,92 @@
 #include "./helper.cpp"
 #include <string>
 #include <unordered_set>
+#include <fstream>
 
+// less than two pieces is illegal, more than 5 is too expensive
+auto constexpr MIN_PIECES_ALLOWED = 2;
+auto constexpr MAX_PIECES_ALLOWED = 5;
+
+// This program will generate an output csv file to be used as a tablebase for the get_next_move file
 int main(int argc, char** argv) {
     int num_ending_pieces;
     int max_pieces_present;
     int depth_to_mate_checked;
+    auto starting_pieces = std::vector<char>{};
 
-    if (argc < 4) {
-        std::cout << "Enter the number of pieces that will be present at checkmate: ";
+    // process/acquire arguments to tablebase generator
+    if (argc < 5) {
+        std::cout << "Enter the number of pieces that will be present at checkmate (integer): ";
         if (not (std::cin >> num_ending_pieces)) {
-            std::cout << "Error: incorrect input type for integer\n";
-            return 0;
+            std::cout << "Error: incorrect input type for integer.\n";
+            return 1;
         }
 
-        std::cout << "Enter the max number of pieces allowed to appear: ";
+        std::cout << "Enter the max number of pieces allowed to appear (integer): ";
         if (not (std::cin >> max_pieces_present)) {
-            std::cout << "Error: incorrect input type for integer\n";
-            return 0;
+            std::cout << "Error: incorrect input type for integer.\n";
+            return 1;
         }
 
-        std::cout << "Enter the max number of unmoves/depth to mate to check: ";
+        std::cout << "Enter the max number of unmoves/depth to mate to check (integer): ";
         if (not (std::cin >> depth_to_mate_checked)) {
-            std::cout << "Error: incorrect input type for integer\n";
-            return 0;
+            std::cout << "Error: incorrect input type for integer.\n";
+            return 1;
+        }
+
+        std::cout   << "Enter the pieces (ignoring position) currently on your board (as a string "
+                    << "with no spaces, containing any of [K, Q, R, B, N, P, k, q, r, b, n, p], "
+                    << "where capital letters are white pieces and lowercase are black pieces, "
+                    << "i.e. FEN notation for pieces, e.g. kKQ). Enter the empty string to test "
+                    << "for all possible pieces: ";
+        std::string temp_string;
+        if (not (std::cin >> temp_string)) {
+            std::cout << "Error: string not entered.\n";
+            return 1;
+        }
+        for (auto i : temp_string) {
+            starting_pieces.emplace_back(i);
         }
     } else {
         num_ending_pieces = std::stoi(argv[1]);
         max_pieces_present = std::stoi(argv[2]);
         depth_to_mate_checked = std::stoi(argv[3]);
+
+        for (auto i : std::string{argv[4]}) {
+            starting_pieces.emplace_back(i);
+        }
     }
 
-    if (num_ending_pieces < 3 or num_ending_pieces > 5) {
-        std::cout << "Error: Bad end piece count, please be greater than 2 and less than 5\n";
+    if (num_ending_pieces < MIN_PIECES_ALLOWED or num_ending_pieces > MAX_PIECES_ALLOWED) {
+        std::cout << "Error: Bad end piece count, please be at least 2 and less than 5.\n";
         return 1;
     } else if (num_ending_pieces > max_pieces_present) {
-        std::cout << "Error: Bad max piece count, is less than end piece count\n";
+        std::cout << "Error: Bad max piece count, is less than end piece count.\n";
         return 1;
     }
+
+    // generate combinations of pieces from which to generate checkmates for retrograde analysis
+    auto piece_combinations = std::vector<std::vector<char>>{};
+    if (starting_pieces.empty()) {
+        piece_combinations = helper::generate_piece_combinations(num_ending_pieces);
+    } else {
+        piece_combinations.emplace_back(starting_pieces);
+    }
+
+    // this is according to n + k - 1 choose k, where n = 10, k = num_ending_pieces, unless pieces are provided
+    std::cout << "There are " << piece_combinations.size() << " combinations of pieces.\n";
 
     // for now we use an unordered set of strings, because afaik gcc has some error when trying to
     // use the overriden equality operator for chess::Board for comparisons in the unordered set
     // can maybe use tries in the future since they are strings?
-    auto piece_combinations = helper::generate_piece_combinations(num_ending_pieces);
-    // this is according to n + k - 1 choose k, where n = 10, k = num_ending_pieces
-    std::cout << piece_combinations.size() << "\n";
-
     auto checkmate_states = std::unordered_set<std::string>();
     for (auto i : piece_combinations) {
-        std::cout << "now generating checkmates for new piece combination\n";
+        auto combination = std::string{};
+        for (auto j : i) {
+            combination.push_back(j);
+        }
+
+        std::cout << "Now generating checkmates for new piece combination: " << combination << "\n";
         auto some_checkmates = helper::generate_checkmates_for_piece_set_for_player(i);
         for (auto j : some_checkmates) {
             checkmate_states.insert(j);
@@ -61,10 +99,9 @@ int main(int argc, char** argv) {
         }
     }
 
-
     // a set of all states with forceable wins for white (regardless of moves to mate)
-    auto all_white_player_states_with_forceable_wins = std::unordered_set<std::string>();
-    all_white_player_states_with_forceable_wins.insert(checkmate_states.begin(), checkmate_states.end());
+    auto states_with_forceable_wins_for_white = std::unordered_set<std::string>();
+    states_with_forceable_wins_for_white.insert(checkmate_states.begin(), checkmate_states.end());
 
     // we use a vector to allow us to store the following:
     // let n = index of element in vector
@@ -79,7 +116,11 @@ int main(int argc, char** argv) {
 
 
     while (depth_to_mate_forced_wins_for_white.size() <= depth_to_mate_checked) {
-        std::cout << "Checking for new move depth, last iteration had this many boards: " << depth_to_mate_forced_wins_for_white.back().size() << "\n";
+        std::cout   << "Checking for new move depth: "
+                    << depth_to_mate_forced_wins_for_white.size()
+                    << ", last iteration had "
+                    << depth_to_mate_forced_wins_for_white.back().size()
+                    << " boards.\n";
         auto temp = std::unordered_set<std::string>();
         for (auto i : depth_to_mate_forced_wins_for_white.back()) {
             if (depth_to_mate_forced_wins_for_white.size() % 2 == 1) {
@@ -89,7 +130,7 @@ int main(int argc, char** argv) {
 
                 for (auto j : possible_predecessor_boards) {
                     // avoid recalculation for states we already know to be winning
-                    if (not all_white_player_states_with_forceable_wins.contains(j)) {
+                    if (not states_with_forceable_wins_for_white.contains(j)) {
                         temp.emplace(j);
                     }
                 }
@@ -98,8 +139,8 @@ int main(int argc, char** argv) {
                 // these are states where whatever move black takes, they will lose in the end
                 auto possible_predecessor_boards = helper::generate_predecessor_board_states(i, false, max_pieces_present);
                 for (auto j: possible_predecessor_boards) {
-                    if (helper::is_forced_win(j, all_white_player_states_with_forceable_wins)) {
-                        if (not all_white_player_states_with_forceable_wins.contains(j)) {
+                    if (helper::is_forced_win(j, states_with_forceable_wins_for_white)) {
+                        if (not states_with_forceable_wins_for_white.contains(j)) {
                             temp.emplace(j);
                         }
                     }
@@ -108,18 +149,29 @@ int main(int argc, char** argv) {
             }
         }
         depth_to_mate_forced_wins_for_white.emplace_back(temp);
-        all_white_player_states_with_forceable_wins.insert(temp.begin(), temp.end());
+        states_with_forceable_wins_for_white.insert(temp.begin(), temp.end());
     }
 
-    auto counter = 0;
-    for (auto i : depth_to_mate_forced_wins_for_white) {
-        std::cout << "\n\n\nDEPTH TO MATE: " << counter << "\n";
-        for (auto j : i) {
-            std::cout << "FEN: " << j << "\n";
-            // helper::print_FEN_as_ASCII_board(j);
+    // output our results to a file storing our results
+    auto output_file = std::ofstream("output.csv");
 
+    // c++ streaming input from a file splits it by whitespace, so we can try format our output
+    // to take advantage of this in the form "depth_to_mate FEN_position_segment player_turn"
+    // for each row of the csv
+    for (auto i = 0; i < depth_to_mate_forced_wins_for_white.size(); ++i) {
+        for (auto j : depth_to_mate_forced_wins_for_white[i]) {
+            auto FEN_position_segment = std::string{};
+
+            auto j_iter = j.begin();
+            for (; *j_iter != ' '; ++j_iter) {
+                FEN_position_segment.push_back(*j_iter);
+            }
+
+            auto player_turn = *(++j_iter);
+
+
+            output_file << i << " " << FEN_position_segment << " " << player_turn << "\n";
         }
-        counter++;
     }
 
     return 0;
